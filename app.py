31fe066a70f554a3f810abb49bcabff7087a2fd7
saem <saem@kaist.ac.kr>
2022-11-11 19:36:43 +0900

    first commit

--- app.py
+++ app.py
@@ -699,7 +699,7 @@ def cemos():
 
                 plt.pie((temp/total_sum)*100, pctdistance=1.15, autopct='%.2f%%', explode = [0.2, 0.2, 0], 
                          textprops={'fontsize': 15})
-                plt.legend(labels = result_df_energy.iloc[:,6:9].columns, fontsize=20, prop=font)
+                plt.legend(loc='upper right', labels = result_df_energy.iloc[:,6:9].columns, fontsize=20, prop=font)
 
                 st.pyplot(fig)
 
