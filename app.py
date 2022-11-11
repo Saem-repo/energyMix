b1d289a14e7c9fc7d386e4258e0eec79054b8e9a
saem <saem@kaist.ac.kr>
2022-11-11 17:55:43 +0900

    first commit

--- app.py
+++ app.py
@@ -698,7 +698,7 @@ def cemos():
                 #          autopct='%.2f%%', explode=exp, rotatelabels=True)
 
                 plt.pie((temp/total_sum)*100, autopct='%.2f%%', textprops={'fontsize': 20})
-                plt.legend(labels = result_df_energy.iloc[:,6:9].columns, prop=font, fontsize=20)
+                plt.legend(labels = result_df_energy.iloc[:,6:9].columns, prop=font, fontsize=65)
 
                 st.pyplot(fig)
 
